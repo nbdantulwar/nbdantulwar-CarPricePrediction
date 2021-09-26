953596fb96cfdf7e3efb9448d25c35fb3d5dc527
nbdantulwar <nitin.star123@gmail.com>
2021-09-26 23:18:04 +0530

    first commit

--- app.py
+++ app.py
@@ -55,19 +55,3 @@ def price_prediction():
 if __name__=='__main__':
     app.run(debug=True)
     #print(db.fetchall())
-
-# …or create a new repository on the command line
-# echo "# nbdantulwar-CarPricePrediction" >> README.md
-# git init
-# git add README.md
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/nbdantulwar/nbdantulwar-CarPricePrediction.git
-# git push -u origin main
-
-# …or push an existing repository from the command line
-# git remote add origin https://github.com/nbdantulwar/nbdantulwar-CarPricePrediction.git
-# git branch -M main
-# git push -u origin main
-# …or import code from another repository
-# You can initialize this repository with code from a Subversion, Mercurial, or TFS project.
